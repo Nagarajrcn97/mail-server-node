4d8c7be64ad01e4bbd8ed19615429279f38acf24
Nagaraj <kumar_nag@am.adecco.net>
2019-07-17 11:57:57 +0530

    commit

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(function(req, res, next) {
      res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
    next();
  });
+ 
 app.post('/email', async (req, res) => {
     const msg = {
         to: 'hello@yopmail.com',
@@ -29,8 +30,21 @@ app.post('/email', async (req, res) => {
     res.send("Hello")
 })
 
-app.listen(4000, function () {
-    console.log('Server listening on port 4000');
+// app.listen(4000, function () {
+//     console.log('Server listening on port 4000');
+// });
+
+ // Initialize the app.
+ var server = app.listen(8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
+
+
+// Generic error handler used by all endpoints.
+// function handleError(res, reason, message, code) {
+// console.log("ERROR: " + reason);
+// res.status(code || 500).json({"error": message});
+// }
 
 // exports.app = functions.https.onRequest(app);
